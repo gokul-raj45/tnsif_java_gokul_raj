04fd1d0d32f4a2a5c210b875328d7ad456e78ef3
gokulraj4545 <gokulraj922005@gmail.com>
2025-09-28 19:21:21 +0530

    Adding another files.

---
+++ tnsif_java_project1/src/pac1/ProductMain.java
@@ -0,0 +1,15 @@
+package pac1;
+
+public class ProductMain {
+
+	public static void main(String[] args) {
+		Product p=new Product();
+		p.setProductId(101);
+		p.setProductName("Laptop");
+		p.setPrice(45000);
+		System.out.println(p);
+		
+
+	}
+
+}
