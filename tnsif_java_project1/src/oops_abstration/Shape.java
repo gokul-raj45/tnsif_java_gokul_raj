5fed1ec27f705eff48d15e6b3532312b9a94ee53
gokulraj4545 <gokulraj922005@gmail.com>
2025-09-29 18:15:29 +0530

    adding abstraction file.

---
+++ tnsif_java_project1/src/oops_abstration/Shape.java
@@ -0,0 +1,17 @@
+package oops_abstration;
+
+
+//abstract class
+public abstract class Shape {
+   protected float area;
+   
+   //abstract method 
+	 abstract void calArea();
+	 
+	 //concrete method
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+}
+
